da2c606557c26cd0c9d8036508913d5f7505cf91
Bill Rossi <bassguitarbill@gmail.com>
2023-11-24 05:52:27 -0500

    Add a command to run a given problem

--- lib/advent_of_code_ruby/input.rb
+++ lib/advent_of_code_ruby/input.rb
@@ -12,7 +12,11 @@ end
 module AdventOfCodeRuby
   module Input
     def load_file(file_name: "input.txt")
-      File.read(self.class.name.underscore.gsub(/\/[a-z_]*$/, "/#{file_name}"))
+      File.read(File.join(lib_folder, self.class.name.underscore.gsub(/\/[a-z_]*$/, "/#{file_name}")))
+    end
+
+    def lib_folder
+      File.join(File.dirname(__FILE__), '../../lib')
     end
   end
 end
