e6ec577aeb1daafa6ce610b16e1bd2d6282edbd5
Bill Rossi <bassguitarbill@gmail.com>
2023-12-01 00:44:26 -0500

    2023 day 1 - make it classy

--- lib/advent_of_code_ruby/year_2023/problem_1/trebuchet.rb
+++ lib/advent_of_code_ruby/year_2023/problem_1/trebuchet.rb
@@ -4,58 +4,81 @@ module AdventOfCodeRuby
   module Year2023
     module Problem1
       class Trebuchet < Problem
+        def part(input, calibration_line_class)
+          input.chomp.split("\n").map { |line| calibration_line_class.new(line).value }.sum
+        end
+
         def part_1(input)
-          input.chomp.split("\n").map do |line|
-            chars = line
-                      .gsub(/[a-z]/, "")
-                      .split("")
-            (chars[0] + chars[-1]).to_i
-          end.sum
+          part(input, CalibrationLine)
         end
 
         def part_2(input)
-          input.chomp.split("\n").map do |line|
-            (first_digit(line) + last_digit(line)).to_i
-          end.sum
+          part(input, AlphaCalibrationLine)
         end
 
-        def is_digit?(c)
-          digits.values.include? c
+        class CalibrationLine
+          attr_reader :text
+          def initialize(text)
+            @text = text
+          end
+
+          def digits
+            text.gsub(/[a-z]/, "").split("")
+          end
+
+          def first_digit
+            digits.first
+          end
+
+          def last_digit
+            digits.last
+          end
+
+          def value
+            (first_digit + last_digit).to_i
+          end
         end
 
-        def digits
-          {
-            "one" => "1",
-            "two" => "2",
-            "three" => "3",
-            "four" => "4",
-            "five" => "5",
-            "six" => "6",
-            "seven" => "7",
-            "eight" => "8",
-            "nine" => "9"
-          }
+        class AlphaCalibrationLine < CalibrationLine
+          def first_digit(str = text)
+            self.class.starts_with_digit?(str) || first_digit(str[1..])
+          end
+
+          def last_digit(str = text)
+            self.class.ends_with_digit?(str) || last_digit(str[0..-2])
+          end
+
+          def self.is_digit?(c)
+            digits.values.include? c
+          end
+
+          def self.digits
+            {
+              "one" => "1",
+              "two" => "2",
+              "three" => "3",
+              "four" => "4",
+              "five" => "5",
+              "six" => "6",
+              "seven" => "7",
+              "eight" => "8",
+              "nine" => "9"
+            }
+          end
+
+          def self.starts_with_digit?(str)
+            return str[0] if is_digit?(str[0])
+            digits.keys.each { |key| return digits[key] if str.start_with?(key) }
+            false
+          end
+
+          def self.ends_with_digit?(str)
+            return str[-1] if is_digit?(str[-1])
+            digits.keys.each { |key| return digits[key] if str.end_with?(key) }
+            false
+          end
         end
 
-        def starts_with_digit?(str)
-          return str[0] if is_digit?(str[0])
-          digits.keys.each { |key| return digits[key] if str.start_with?(key) }
-          nil
-        end
-
-        def first_digit(str)
-          starts_with_digit?(str) || first_digit(str[1..])
-        end
-
-        def ends_with_digit?(str)
-          return str[-1] if is_digit?(str[-1])
-          digits.keys.each { |key| return digits[key] if str.end_with?(key) }
-          nil
-        end
-
-        def last_digit(str)
-          ends_with_digit?(str) || last_digit(str[0..-2])
-        end
       end
     end
   end
